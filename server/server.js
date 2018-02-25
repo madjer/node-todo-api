70c30396e315c67dcae4140cf02d5997e4f97063
Madjer <madjerbo@gmail.com>
2018-02-25 20:06:38 -0300

    Mongoose validators, types and defaults

--- server/server.js
+++ server/server.js
@@ -5,24 +5,46 @@ mongoose.connect('mongodb://localhost:27017/TodoApp');
 
 let Todo = mongoose.model('Todo', {
     text: {
-        type: String
+        type: String,
+        required: true,
+        minlength: 1,
+        trim: true    
     }, 
     completed: {
-        type: Boolean
+        type: Boolean,
+        default: false
     },
     completedAt: {
-        type: Number
+        type: Number,
+        default: null
     }
 });
 
-let newTodo = new Todo({
-    text: 'Pedir o jantar',
-    completed: false,
-    completedAt: null
+let User = mongoose.model('User', {
+    email: {
+        type: String,
+        required: true,
+        minlength: 3,
+        trim: true    
+    }
 });
 
-newTodo.save().then((doc)=>{
-    console.log('Saved todo', doc);
+// let newTodo = new Todo({
+//     text: 'Pedir o jantar',
+//     completed: false,
+//     completedAt: null
+// });
+
+// newTodo.save().then((doc)=>{
+//     console.log('Saved todo', doc);
+// }, (e) => {
+//     console.log('Unable to save todo', e);
+// })
+let user = new User({
+    email: 'madjerbo@gmail.com'
+});
+user.save().then((doc) => {
+    console.log('User saved', doc);
 }, (e) => {
-    console.log('Unable to save todo', e);
-})
+    console.log('Unable to save user', e);
+});
