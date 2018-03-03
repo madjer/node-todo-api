81a762ae5bdf211c59dddaaf61cbaa41270035ff
Madjer <madjerbo@gmail.com>
2018-03-03 17:20:32 -0300

    Setting up the user Model

--- server/models/user.js
+++ server/models/user.js
@@ -1,10 +1,32 @@
-let mongoose = require('mongoose');
+const mongoose = require('mongoose');
+const validator = require('validator') 
 let User = mongoose.model('User', {
     email: {
         type: String,
         required: true,
         minlength: 3,
-        trim: true    
-    }
+        trim: true,
+        unique: true,
+        validade: {
+            validator: validator.isEmail,
+            message: '{VALUE} is not a valid e-mail'
+        }   
+    },
+    password: {
+        type: String,
+        requeire: true,
+        minlength: 6
+    },
+    tokens: [{
+        access: {
+            type:String,
+            requiered: true
+        },
+        token: {
+            type: String,
+            requiered: true
+        }
+    }]
 });
+
 module.exports = {User}
